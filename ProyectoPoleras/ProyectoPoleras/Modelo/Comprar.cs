ae53e3c94108e4bba60bed4893d9013bb9e34da4
CamiloOgalde <104463817+CamiloOgalde@users.noreply.github.com>
2022-05-18 15:41:59 -0400

    Add files via upload

---
+++ ProyectoPoleras/ProyectoPoleras/Modelo/Comprar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPoleras
+{
+    internal class Comprar
+    {
+		private int id;
+
+        public int Id { get => id; set => id = value; }
+
+        public Comprar()
+        {
+
+        }
+
+        public Comprar(int _id)
+        {
+            Id = _id;
+            
+        }
+        public void GenerarFactura()
+        {
+            Console.WriteLine("Generando Factura..");
+        }
+    }
+}
---
+++ ProyectoPoleras/ProyectoPoleras/Modelo/Genero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPoleras
+{
+    internal class Genero
+    {
+		private int id;
+		private String nomb_genero;
+
+        public int Id { get => id; set => id = value; }
+        public string Nomb_genero { get => nomb_genero; set => nomb_genero = value; }
+
+        public Genero()
+        {
+
+        }
+
+        public Genero(int _id, string _nomb_genero)
+        {
+            Id = _id;
+            Nomb_genero = _nomb_genero;
+           
+        }
+    }
+}
