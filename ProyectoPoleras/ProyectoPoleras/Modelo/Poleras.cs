744e2cf6918e1cdef03cab6f6ff3e1d5b252ab46
CamiloOgalde <104463817+CamiloOgalde@users.noreply.github.com>
2022-05-24 16:36:34 -0400

    Add files via upload

---
+++ ProyectoPoleras/ProyectoPoleras/Modelo/Poleras.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPoleras
+{
+    internal class Poleras
+    {
+		private int id;
+		private String nombre;
+		private String talla;
+		private String descripcion;
+
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Talla { get => talla; set => talla = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+
+        public Poleras()
+        {
+          
+        
+        }
+
+        public Poleras(int _id, string _nombre, string _talla, string _descripcion)
+        {
+            Id = _id;
+            Nombre = _nombre;
+            Talla = _talla;
+            Descripcion = _descripcion;
+           
+        }
+    }
+}
